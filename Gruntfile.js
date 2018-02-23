a497ca0f4d0347a945f6ad3f7a2fd3a266b04e0f
Oliver Pohl <opohl@bbaw.de>
2018-02-23 15:30:27 +0100

    Recherchekonfiguration hinzugefügt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
-// const path = require('path');
+const path = require('path');
+
+const rBaseConfiguration = path.resolve(__dirname, 'rconfig/Win/Backup/BaseConfiguration');
 
 module.exports = function (grunt) {
   grunt.initConfig({
@@ -6,7 +8,8 @@ module.exports = function (grunt) {
 
     // Copy task
     copy: {
-      main: {
+
+      editor: {
         files: [
           {
             expand: true,
@@ -16,17 +19,51 @@ module.exports = function (grunt) {
           },
         ],
       },
+
+      recherche: {
+        files: [
+          {
+            expand: true,
+            cwd: 'config/',
+            src: ['**'],
+            dest: 'rconfig',
+          },
+        ],
+      },
     },
 
     // Rename folders
     rename: {
-      moveThis: {
+      fsconfig: {
         src: '*.201*',
         dest: 'temp_config',
         options: {
           ignore: true,
         },
       },
+
+      rSidePanels: {
+        src: path.resolve(rBaseConfiguration, 'fsSidePanels_recherche.xml'),
+        dest: path.resolve(rBaseConfiguration, 'fsSidePanels.xml'),
+        options: {
+          ignore: true,
+        },
+      },
+      rArchives: {
+        src: path.resolve(rBaseConfiguration, 'fsArchives_recherche.xml'),
+        dest: path.resolve(rBaseConfiguration, 'fsArchives.xml'),
+        options: {
+          ignore: true,
+        },
+      },
+      rActionItems: {
+        src: path.resolve(rBaseConfiguration, 'fsActionItems_recherche.xml'),
+        dest: path.resolve(rBaseConfiguration, 'fsActionItems.xml'),
+        options: {
+          ignore: true,
+        },
+      },
+
     },
 
     // Delete unneccessary folders
@@ -36,9 +73,9 @@ module.exports = function (grunt) {
 
     // Compress
     compress: {
-      main: {
+      editor: {
         options: {
-          archive: 'dist/<%= pkg.name %>_<%= pkg.version %>.zip'
+          archive: 'dist/<%= pkg.name %>_<%= pkg.version %>.zip',
         },
         files: [
           {
@@ -46,6 +83,21 @@ module.exports = function (grunt) {
             cwd: 'config/',
             src: ['**'],
             dest: '<%= pkg.name %>_<%= pkg.version %>/',
+          },
+        ],
+      },
+
+      // Compress recherche
+      recherche: {
+        options: {
+          archive: 'dist/<%= pkg.name %>_<%= pkg.version %>_recherche.zip',
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'rconfig/',
+            src: ['**'],
+            dest: '<%= pkg.name %>_<%= pkg.version %>_recherche/',
           },
         ],
       },
@@ -72,5 +124,27 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-xmlpoke');
 
-  grunt.registerTask('default', ['rename', 'copy', 'xmlpoke', 'clean', 'compress', 'bump-only']);
+  //grunt.registerTask('default', ['rename', 'copy', 'xmlpoke', 'clean', 'compress', 'bump-only']);
+
+  grunt.registerTask('recherche', [
+    'rename:fsconfig',
+    'copy:editor',
+    'copy:recherche',
+    'rename:rArchives',
+    'rename:rActionItems',
+    'rename:rSidePanels',
+    'clean',
+    'compress:recherche',
+  ]);
+
+  grunt.registerTask('editor', [
+    'rename:fsconfig',
+    'copy:editor',
+    'xmlpoke',
+    'clean',
+    'compress:editor',
+    'bump-only',
+  ]);
+
+  grunt.registerTask('default', ['editor', 'recherche']);
 };
